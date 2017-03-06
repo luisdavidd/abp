aab1fa4050a76128f1b46b010a1546131631d34c
luisdavidd <luisdadiaz18@hotmail.com>
2017-03-06 16:42:40 -0500

    Changes on controllers.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,10 +50,18 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   #Perform Delivery actions
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      user_name: "abpbacs@gmail.com",
+      password: "abppassw",
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      enable_starttls_auto: true,
+  }
 end
