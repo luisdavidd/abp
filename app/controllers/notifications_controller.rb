3d1491507d4a59d9d15b85656cdc2eb22cdab4b1
Sorelys <sorboy11@gmail.com>
2017-07-04 15:02:38 -0700

    Notifications 50%

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,12 @@ class NotificationsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		@notifications = Notifications.where(recipient: current_user).unread
+		nrcs = UserSubject.connection.execute("SELECT subject_id FROM user_subjects where user_id=36;")
+		query = "recipient_id="+current_user.id.to_s
+		nrcs.each do |nrc|
+			query = query + " or recipient_id="+nrc[0].to_s
+		end
+		@notifications = Notification.where(query).unread
 	end
 
 end
