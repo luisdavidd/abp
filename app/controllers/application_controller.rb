b4a5548184c21627b08978752911ac3e5c14c385
Juan Camilo Gomez Santofimio <juan.camilo.gomez95@gmail.com>
2019-10-13 16:37:46 -0500

    fix in create user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :codigo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :codigo, :teacher])
   	devise_parameter_sanitizer.permit(:account_update,keys:[:current_password,:avatar]) 
   end
 
